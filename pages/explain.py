f60a41f04a935f778dcdbee6cd6c0c5b4f478bcb
Gregório Victor Rodrigues <gregorio.rodrigues@gmail.com>
2025-03-10 14:51:22 -0300

    Update explain.py

--- pages/explain.py
+++ pages/explain.py
@@ -8,31 +8,31 @@ import plotly.graph_objects as go
 from h2o.estimators import H2OGenericEstimator
 
 #Baixando modelo
-    try:
-        # URL do modelo no Google Drive
-        file_id = "1IEGIuHt1l8xwR_Jl5J_fuKf0h5Fkdwx2"  # Substitua pelo ID do seu arquivo
-        url = f"https://drive.google.com/uc?id={file_id}"
+try:
+    # URL do modelo no Google Drive
+    file_id = "1IEGIuHt1l8xwR_Jl5J_fuKf0h5Fkdwx2"  # Substitua pelo ID do seu arquivo
+    url = f"https://drive.google.com/uc?id={file_id}"
 
-        # Nome do arquivo para salvar localmente
-        model_filename = "modelo_em_mojo.zip"
+    # Nome do arquivo para salvar localmente
+    model_filename = "modelo_em_mojo.zip"
 
-        # Baixar o modelo
-        @st.cache_resource
-        def download_model():
-            gdown.download(url, model_filename, quiet=False)
-            return model_filename
+    # Baixar o modelo
+    @st.cache_resource
+    def download_model():
+        gdown.download(url, model_filename, quiet=False)
+        return model_filename
         
-        # Baixar o modelo
-        download_model()
+    # Baixar o modelo
+    download_model()
 
-        # Iniciar o H2O e carregar o modelo
-        h2o.init()
+    # Iniciar o H2O e carregar o modelo
+    h2o.init()
 
-        # Carregar o modelo H2O
-        model = h2o.import_mojo(model_filename)
-        st.session_state['model'] = model
+    # Carregar o modelo H2O
+    model = h2o.import_mojo(model_filename)
+    st.session_state['model'] = model
         
-        st.write("✅ Modelo carregado com sucesso!")
+    st.write("✅ Modelo carregado com sucesso!")
         
-    except Exception as e:
+except Exception as e:
         st.write("❌ Erro ao carregar modelo...", str(e))
